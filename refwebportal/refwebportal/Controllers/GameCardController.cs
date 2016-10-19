4bbe6de240aba39c55a11dc65e878530ccc87f8f
Jeremy Cook <jeremy.cook.llb@live.com>
2016-10-19 17:38:53 +1300

    creating gamecard

---
+++ refwebportal/refwebportal/Controllers/GameCardController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using refwebportal;
+using refwebportal.Models;
+
+namespace refwebportal.Controllers
+{
+    public class GameCardController : Controller
+    {
+        private FROdataEntities3 db = new FROdataEntities3();
+
+        // GET: GameCard
+        public ActionResult Index(int? id, int? gameTeamID)
+        {
+            var viewModel = new GamesIndexData();
+            viewModel.Games = db.Games
+                .OrderBy(i => i.GameDateTime);
+
+            if (id != null)
+            {
+                ViewBag.GameID = id.Value;
+                viewModel.GameTeams = viewModel.Games.Where(
+                    i => i.Id == id.Value).Single().GameTeams;
+            }
+
+            if (gameTeamID != null)
+            {
+                ViewBag.GameTeamID = gameTeamID.Value;
+                viewModel.GamePlayers = viewModel.Games.Where(
+                    x => x.Id == gameTeamID).Single().GamePlayers;
+            }
+
+            return View(viewModel);
+        }
+
+        // GET: GameCard/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = await db.Games.FindAsync(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // GET: GameCard/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GameCard/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Description,GameDateTime")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Games.Add(game);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(game);
+        }
+
+        // GET: GameCard/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = await db.Games.FindAsync(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // POST: GameCard/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Description,GameDateTime")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(game).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(game);
+        }
+
+        // GET: GameCard/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = await db.Games.FindAsync(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // POST: GameCard/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Game game = await db.Games.FindAsync(id);
+            db.Games.Remove(game);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ refwebportal/refwebportal/Models/GamesIndexData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace refwebportal.Models
+{
+    public class GamesIndexData
+    {
+        public IEnumerable<Game> Games { get; set; }
+        public IEnumerable <GameTeam> GameTeams { get; set; }
+        public IEnumerable<GamePlayer> GamePlayers { get; set; }
+    }
+}
